a59209ea141d73110ae19fa73913d6b42c38d753
adwon24 <adwon.manager@gmail.com>
2025-06-26 16:20:22 +0900

    [수정] point4u 호출 코드

--- app/src/main/java/com/point4u/sample/MainActivity.kt
+++ app/src/main/java/com/point4u/sample/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.adwon.point4u.Point4U
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -26,8 +27,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startPoint4U() {
-
+        val point4u = Point4U()
+        point4u.startPoint4U(
+            activity = this@MainActivity,
+            companyCode = POINT4U_TEST_COMPANY_CODE,
+            sdkCode = POINT4U_TEST_SDK_CODE,
+            memberId = POINT4U_TEST_MEMBER_ID,
+            age = POINT4U_TEST_MEMBER_AGE,
+            gender = POINT4U_TEST_MEMBER_GENDER
+        )
     }
 
 
+    companion object {
+        const val POINT4U_TEST_COMPANY_CODE = "2e72dbd2-e246-4d4f-9d29-472712232c72"
+        const val POINT4U_TEST_SDK_CODE = "78dbdded-f2be-469a-8444-79a8b0334c97"
+        const val POINT4U_TEST_MEMBER_ID = "sample_1234"
+
+        const val POINT4U_TEST_MEMBER_AGE = "28"
+        const val POINT4U_TEST_MEMBER_GENDER = "1" // 남성 1, 여성 0
+    }
 }
